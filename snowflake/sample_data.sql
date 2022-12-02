f9f1c949d32cf3a179cda33e5a39035e369e2049
Radosław Szmit <radoslawszmit@gmail.com>
2022-12-02 22:29:56 +0100

    Snowflake sample queries

--- snowflake/sample_data.sql
+++ snowflake/sample_data.sql
@@ -1,7 +1,103 @@
+USE DATABASE "SNOWFLAKE_WAREHOUSE";
+use schema snowflake_warehouse.public;
 
-USE DATABASE "SNOWFLAKE_SAMPLE_DATA";
+ALTER SESSION SET USE_CACHED_RESULT = FALSE;
 
-ALTER SESSION SET USE_CACHED_RESULT = TRUE;
+SELECT avg(o_totalprice), max(o_orderdate), min(o_orderdate)
+FROM ORDERS;
+
+SELECT distinct o_orderstatus
+FROM ORDERS;
+
+SELECT distinct o_orderpriority
+FROM ORDERS;
+
+-- top 10 clients by value
+SELECT sum(o_totalprice) as totalpricesum, count(1) as numberoforders, o.o_custkey
+FROM ORDERS o
+left join customer c on c.c_custkey = o.o_custkey
+group by o_custkey
+order by totalpricesum desc
+limit 10;
+
+-- better query and performance
+select t1.totalpricesum, numberoforders, c.c_name, c.c_custkey
+from (
+  SELECT sum(o_totalprice) as totalpricesum, count(1) as numberoforders, o_custkey
+    FROM ORDERS o
+    group by o_custkey
+) as t1
+left join customer c on c.c_custkey = t1.o_custkey
+order by totalpricesum desc
+limit 10;
+
+-- biggest client to variable
+set biggestclientid=104056177;
+
+--
+select *
+from orders o
+left join lineitem l ON o_orderkey = l_orderkey
+where o_custkey = $biggestclientid;
+
+
+-- best products
+select l_partkey, sum(l_quantity) as totalquantity, sum(l_extendedprice) as totalprice, count(1) as totalorders
+from lineitem l
+group by l_partkey
+order by totalprice desc
+limit 10;
+
+
+-- best suppliers
+select l_suppkey, sum(l_quantity) as totalquantity, sum(l_extendedprice) as totalprice, count(1) as totalorders
+from lineitem l
+group by l_suppkey
+order by totalprice desc
+limit 10;
+
+-- part vs supplier
+select ps_partkey, count(*) as numberofsuppliers
+from partsupp
+group by ps_partkey
+order by numberofsuppliers desc, ps_partkey desc
+limit 10;
+
+set partkey = 20000000;
+select l_extendedprice / l_quantity, l.*
+from partsupp ps
+left join part p on p_partkey = ps_partkey
+left join supplier s on ps_suppkey = s_suppkey
+left join lineitem l on (l_partkey = ps_partkey and l_suppkey = ps_suppkey)
+where p_partkey = $partkey
+limit 100;
+
+
+-- The query lists totals for extended price, discounted extended price, discounted extended price plus tax, average quantity, average extended price, and average discount.
+-- These aggregates are grouped by RETURNFLAG and LINESTATUS, and listed in ascending order of RETURNFLAG and LINESTATUS.
+-- A count of the number of line items in each group is included:
+select
+       l_returnflag,
+       l_linestatus,
+       sum(l_quantity) as sum_qty,
+       sum(l_extendedprice) as sum_base_price,
+       sum(l_extendedprice * (1-l_discount)) as sum_disc_price,
+       sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge,
+       avg(l_quantity) as avg_qty,
+       avg(l_extendedprice) as avg_price,
+       avg(l_discount) as avg_disc,
+       count(*) as count_order
+ from
+       lineitem
+ where
+       l_shipdate <= dateadd(day, -90, to_date('1998-12-01'))
+ group by
+       l_returnflag,
+       l_linestatus
+ order by
+       l_returnflag,
+       l_linestatus;
+
 
 SELECT "NATION"."N_NAME" AS "N_NAME"
 FROM "TPCH_SF1000"."ORDERS" "ORDERS"
