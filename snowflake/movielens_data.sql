a3e40054a1772c59da56e649149035f15755deaa
Radosław Szmit <radoslawszmit@gmail.com>
2023-10-13 21:34:36 +0200

    Movielens

--- snowflake/movielens_data.sql
+++ snowflake/movielens_data.sql
@@ -6,7 +6,30 @@ use schema public;
 
 show tables;
 
+--- Movies
 
+drop table if exists movies;
+
+CREATE TABLE movies (
+    movieid int,
+    title varchar,
+    genres varchar
+);
+
+copy into movielens.public.movies (
+    movieid, title, genres
+)
+from 's3://radek-datasets-public/movielens/demo/movies/movies.dat'
+file_format = (
+  type = 'CSV'
+  field_delimiter = '@'
+  skip_header = 0
+);
+
+select * from ratings limit 10;
+select count(*) from ratings;
+
+--- RATINGS
 
 drop table if exists ratings;
 
@@ -14,7 +37,7 @@ CREATE TABLE ratings (
     userid int,
     movieid int,
     rating double,
-    timestamp int
+    timestamp timestamp
 );
 
 copy into movielens.public.ratings (
@@ -30,6 +53,7 @@ file_format = (
 select * from ratings limit 10;
 select count(*) from ratings;
 
+--- TAGS
 
 drop table if exists tags;
 
@@ -37,7 +61,7 @@ CREATE TABLE tags (
     userid int,
     movieid int,
     tag varchar,
-    timestamp int
+    timestamp timestamp
 );
 
 copy into movielens.public.tags (
